0d05fd0c1957451eeaa2c38dfe48688f09f7929c
Tim Winsley <carpentertimw@gmail.com>
2023-10-06 18:21:24 -0400

    Fill out purchase class

--- src/main/java/com/winsley/sandwich/services/Purchase.java
+++ src/main/java/com/winsley/sandwich/services/Purchase.java
@@ -1,4 +1,15 @@
 package com.winsley.sandwich.services;
 
+import com.winsley.sandwich.entities.Cart;
+import com.winsley.sandwich.entities.CartItem;
+import com.winsley.sandwich.entities.Customer;
+import lombok.Data;
+
+import java.util.Set;
+
+@Data
 public class Purchase {
+    private Customer customer;
+    private Cart cart;
+    private Set<CartItem> cartItem;
 }
