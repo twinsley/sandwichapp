726a4ce19b3eb8258bd4ce442c3e2bc14f14bfdb
Tim Winsley <carpentertimw@gmail.com>
2023-10-15 18:34:48 -0400

    Add search section

--- src/App/src/app/app.component.spec.ts
+++ src/App/src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'd288-front-end'`, () => {
+  it(`should have as title 'Sandwich Shop'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('d288-front-end');
+    expect(app.title).toEqual('Sandwich Shop');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('d288-front-end app is running!');
+    expect(compiled.querySelector('.content span')?.textContent).toContain('Sandwich Shop app is running!');
   });
 });
