51b4f35de3564e960bb5c09569976c7d40422448
Tim Winsley <carpentertimw@gmail.com>
2023-10-21 16:23:49 -0400

    Finish build script

--- BuildScript.ps1
+++ BuildScript.ps1
@@ -1,3 +1,9 @@
+$registry = Read-Host -Prompt "Please enter the Azure Container Registry name"
+az login
+az acr login -n $registry
+$appName = "$registry.azurecr.io/sandwichapp"
+$appNameTagged = "$appName`:latest"
+Write-Host $appNameTagged
 .\mvnw.cmd package
-docker build -t "sandwichapp" .
-docker run -d -p 80:8080 --name "sandwichapp" sandwichapp
+docker build -t $appName .
+docker push $appNameTagged
